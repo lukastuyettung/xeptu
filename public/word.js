67b748fd3b8ef245aef17b82353ee22b8caef35a
lukastuyettung <anhnguyen1122@gmail.com>
2025-09-24 14:23:49 +0700

    Update word.js

--- public/word.js
+++ public/word.js
@@ -1,12 +1,7 @@
-// Danh sách các từ ghép 2 tiếng
+// words.js
 const words = [
   "Mưa ngâu", "Trái tim", "Ngôi sao", "Bầu trời", "Con mèo",
   "Tình yêu", "Hoa hồng", "Biển xanh", "Ngọc trai", "Ánh sáng",
   "Bàn tay", "Ngọn gió", "Trái đất", "Sân trường", "Mặt trăng",
   "Ngọn núi", "Dòng sông", "Ngôi nhà", "Cánh đồng", "Cây cầu"
 ];
-
-// Để file khác (index.html) có thể dùng
-if (typeof module !== "undefined") {
-  module.exports = words;
-}
